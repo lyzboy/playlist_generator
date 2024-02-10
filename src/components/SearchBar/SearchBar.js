e71706a9d7385e400c824c8884731777cecf9573
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-09 21:09:34 -0700

    Finish testing the SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-export default function SearchBar() {
+export default function SearchBar({ setSearchValue, handleSearch }) {
+    function handleSubmit(e) {
+        e.preventDefault();
+        handleSearch();
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     name="song-input"
                     placeholder="Enter song title"
+                    onChange={(e) => setSearchValue(e.target.value)}
                 />
                 <button>Search</button>
             </form>
--- src/components/SearchBar/SearchBar.test.js
+++ src/components/SearchBar/SearchBar.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SearchBar from "./SearchBar";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("SearchBar Component", () => {
     it("has search bar component that is rendered", () => {
@@ -32,5 +32,39 @@ describe("SearchBar Component", () => {
 
         //Assert
         expect(inputElement).toBeInTheDocument();
+    });
+
+    it("sets the search value using setSearchValue on input change", () => {
+        //Arrange
+        // create a mock setSearchValue function
+        const mockSetSearchValue = jest.fn();
+        render(<SearchBar setSearchValue={mockSetSearchValue} />);
+
+        //Act
+        fireEvent.change(screen.getByPlaceholderText("Enter song title"), {
+            target: { value: "Imagine" },
+        });
+
+        //Assert
+        expect(mockSetSearchValue).toHaveBeenCalledWith("Imagine");
+    });
+
+    it("calls the search function when the button is clicked", () => {
+        //Arrange
+        // create a mock setSearchValue function
+        const mockSetSearchValue = jest.fn();
+        const mockSearch = jest.fn();
+        render(
+            <SearchBar
+                setSearchValue={mockSetSearchValue}
+                handleSearch={mockSearch}
+            />
+        );
+
+        //Act
+        fireEvent.click(screen.getByRole("button", { name: /search/i }));
+
+        //Assert
+        expect(mockSearch).toHaveBeenCalled();
     });
 });
