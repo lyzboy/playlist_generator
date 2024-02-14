47f81a9d18b0561de797292bbb8c74a8fe5e30e2
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-13 22:26:02 -0700

    start building token function

--- src/utils/spotifyAPI.js
+++ src/utils/spotifyAPI.js
@@ -1,25 +1,29 @@
-var client_id = '7bd3a135912c46449b2387c6a65ec687';
-var redirect_uri = 'http://localhost:3000/';
+var client_id = "7bd3a135912c46449b2387c6a65ec687";
+var redirect_uri = "http://localhost:3000/";
 
-var scope = 'user-read-private user-read-email';
+var scope = "user-read-private user-read-email";
 
-var url = 'https://accounts.spotify.com/authorize';
-url += '?response_type=token';
-url += '&client_id=' + encodeURIComponent(client_id);
-url += '&scope=' + encodeURIComponent(scope);
-url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+var url = "https://accounts.spotify.com/authorize";
+url += "?response_type=token";
+url += "&client_id=" + encodeURIComponent(client_id);
+url += "&scope=" + encodeURIComponent(scope);
+url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
 
-
+let token;
+let expireTime;
 
 const Spotify = {
-    getToken(){
-        window.location = url;
+    getToken() {
+        if (!token) {
+            window.location = url;
+            token = window.location.href.match(/acess_token=([^&]*)/);
+        }
     },
 
-    search(term){
+    search(term) {
         const returnUrl = this.getToken();
         console.log(returnUrl);
-    }
-}
+    },
+};
 
 export default Spotify;
