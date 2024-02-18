c67515fd0c78c2dbf7874a15074dbce47b1a578c
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-02-18 11:03:42 -0700

    Style Playbutton compnent

--- src/components/PlayButton/PlayButton.js
+++ src/components/PlayButton/PlayButton.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import styles from './PlayButton.module.css';
+import styles from "./PlayButton.module.css";
 
-export default function PlayButton(){
-    return(
+export default function PlayButton() {
+    return (
         <div className={styles.playButton}>
-            <span className="material-symbols-outlined">play_circle</span>
-            <span className="material-symbols-outlined">pause_circle</span>
+            {true ? (
+                <span className="material-symbols-outlined">play_circle</span>
+            ) : (
+                <span className="material-symbols-outlined">pause_circle</span>
+            )}
         </div>
     );
 }
