ba09c15b6bd8d421d3b8b340edf500f898a1e894
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-09-06 11:23:51 -0600

    feat: add notification of playlist creation.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,25 +2,31 @@ import React from "react";
 
 import styles from "./SearchBar.module.css";
 
-export default function SearchBar({ setSearchValue, handleSearch,searchValue }) {
+export default function SearchBar({
+    setSearchValue,
+    handleSearch,
+    searchValue,
+}) {
     function handleSubmit(e) {
         e.preventDefault();
         handleSearch();
     }
     return (
-
-            <div className={styles.searchBar}>
-                <input className={styles.searchInput}
-                    type="text"
-                    name="song-input"
-                    placeholder="Enter song title"
-                    onChange={(e) => setSearchValue(e.target.value)}
-                    value= {searchValue}
-                    onKeyDown={(e) =>{
-                        if(e.key === "Enter") handleSubmit(e);
-                    }}
-                />
-                <button onClick={handleSubmit} className={styles.searchButton}>Search</button>
-            </div>
+        <div className={styles.searchBar}>
+            <input
+                className={styles.searchInput}
+                type="text"
+                name="song-input"
+                placeholder="Enter song title or artist"
+                onChange={(e) => setSearchValue(e.target.value)}
+                value={searchValue}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") handleSubmit(e);
+                }}
+            />
+            <button onClick={handleSubmit} className={styles.searchButton}>
+                Search
+            </button>
+        </div>
     );
 }
--- src/utils/spotifyAPI.js
+++ src/utils/spotifyAPI.js
@@ -3,40 +3,39 @@ const Spotify = {
         // Retrieve the token and expiration time from sessionStorage
         let token = sessionStorage.getItem("token");
         let expireTime = Number(sessionStorage.getItem("expireTime"));
-    
+
         // Check if the token is not set or if it has expired
         if (!token || expireTime < new Date().getTime()) {
             var client_id = process.env.REACT_APP_CLIENT_ID;
-    
-            var redirect_uri = "https://spotimix.netlify.app/";
-    
+
+            var redirect_uri = "http://localhost:3000/";
+
             var scope = "playlist-modify-public";
-    
+
             var url = "https://accounts.spotify.com/authorize";
             url += "?response_type=token";
             url += "&client_id=" + client_id;
             url += "&scope=" + encodeURIComponent(scope);
             url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
-    
+
             // If so, redirect to the authorization URL
             window.location = url;
-    
+
             // Extract the token and expiration time from the URL
             token = window.location.href.match(/access_token=([^&]*)/)[1];
             const expiresIn =
                 window.location.href.match(/expires_in=([^&]*)/)[1];
-    
+
             // Calculate the expiration time and store it in sessionStorage
             expireTime = new Date().getTime() + Number(expiresIn) * 1000;
             sessionStorage.setItem("expireTime", expireTime.toString());
-    
+
             // Store the token in sessionStorage
             sessionStorage.setItem("token", token);
         }
         // Return the token
         return token;
     },
-    
 
     async search(term) {
         // if token is not create, search doesn't happen
@@ -115,7 +114,7 @@ const Spotify = {
             method: "POST",
             headers: {
                 Authorization: `Bearer ${userToken}`,
-                'Content-type': 'application/json',
+                "Content-type": "application/json",
             },
             body: JSON.stringify({
                 name: playlistName,
@@ -157,7 +156,7 @@ const Spotify = {
             method: "POST",
             headers: {
                 Authorization: `Bearer ${userToken}`,
-                'Content-type': 'application/json',
+                "Content-type": "application/json",
             },
             body: JSON.stringify({
                 uris: trackURIs,
