e64f07016f8eb8b5cd4eccca346c8d30938633fb
lyzboy <13478602+lyzboy@users.noreply.github.com>
2024-09-07 21:08:32 -0600

    Add blink notify to playlist name

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import styles from "./Playlist.module.css";
-
 import Tracklist from "../Tracklist/Tracklist";
 import Notifier from "../Notifier/Notifier";
 
@@ -13,28 +11,62 @@ export default function Playlist({
     handleSavePlaylist,
 }) {
     const [hidden, setHidden] = useState(true);
-    const validatePlaylistName = async () => {
-        try {
-            if (playlistName === "New Playlist") {
-                setHidden(false);
-                await setTimeout(() => {
-                    setHidden(true);
-                }, 5000);
-            } else {
+    const [blink, setBlink] = useState(false);
+    const [blinkingInterval, setBlinkingInterval] = useState(null);
+    const [isBlinking, setIsBlinking] = useState(false);
+
+    useEffect(() => {
+        setBlink(!blink);
+    }, [isBlinking]);
+
+    const validatePlaylistName = () => {
+        if (playlistName === "New Playlist") {
+            setHidden(false);
+            handleBlink();
+            setTimeout(() => {
                 setHidden(true);
-            }
-        } catch (err) {
-            console.log(err);
+                stopBlinking(); // Stop blinking after 5 seconds
+            }, 5000);
+        } else {
+            setHidden(true);
+            stopBlinking();
+        }
+    };
+
+    const handleBlink = () => {
+        // Start blinking by setting up an interval
+        if (!blinkingInterval) {
+            const intervalId = setInterval(() => {
+                setIsBlinking((prev) => !prev); // Toggle blink every 500ms
+                console.log("blinking");
+            }, 500);
+            setBlinkingInterval(intervalId);
+        }
+    };
+
+    const stopBlinking = () => {
+        // Clear the blinking interval
+        if (blinkingInterval) {
+            clearInterval(blinkingInterval);
+            setBlinkingInterval(null);
+            setIsBlinking(false); // Ensure the element returns to non-blinking state
+            setBlink(false);
         }
     };
 
     return (
         <div className={styles.playlist}>
             <input
-                className={styles.playlist_input}
+                className={` ${styles.playlist_input} ${
+                    blink ? styles.blink : ""
+                }`}
                 type="text"
                 value={playlistName}
-                onChange={(e) => setPlaylistName(e.target.value)}
+                onChange={(e) => {
+                    setPlaylistName(e.target.value);
+                    stopBlinking();
+                    if (blink) setBlink(false);
+                }}
             />
             <Tracklist
                 tracks={playlist}
